9b7e95d054f30c05b423668be514df73ff98af56
Moein Arabi <moein.mo81@gmail.com>
2025-05-18 19:54:28 +0330

    Refactor on schema
    - Add the read step to subclass of function step
    - Add function parameters to the schema

--- schema/step.py
+++ schema/step.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from devices.base import Instrument
 
 
@@ -19,12 +21,11 @@ class Step:
             ValueError: If the step data is invalid.
         """
         condition = ('command' in self.data,
-                     'function' in self.data,
-                     'read' in self.data)
+                     'function' in self.data)
         if sum(condition) != 1:
-            raise ValueError('Step must have exactly one of command, function, or read')
+            raise ValueError('Step must have exactly one of command, function')
         
-    def execute(self, instrument: Instrument):
+    def execute(self, instrument: Instrument) -> Any:
         """
         Executes the step on the given instrument.
         
@@ -45,8 +46,6 @@ class Step:
             return CommandStep.from_dict(data)
         elif 'function' in data:
             return FunctionStep.from_dict(data)
-        elif 'read' in data:
-            return ReadStep.from_dict(data)
         else:
             raise ValueError('Unknown step type')
         
@@ -68,7 +67,8 @@ class FunctionStep(Step):
     def __init__(self, data):
         super().__init__(data)
         self.function = data['function']
-    
+        self.params = data.get('params', {})
+
     @staticmethod    
     def from_dict(data: dict) -> 'FunctionStep':
         """Factory method to create the correct FunctionStep subclass."""
@@ -76,27 +76,29 @@ class FunctionStep(Step):
         function = data['function']
         if function == 'measure_voltage':
             return MeasureVoltage.from_dict(data)
+        elif function == 'read_response':
+            return ReadStep.from_dict(data)
         return FunctionStep(data)        
         # TODO: raise ValueError(f'Unknown function type: {function}')
     
-    def execute(self, instrument):
+    def execute(self, instrument) -> Any:
         method = getattr(instrument, self.function)
-        method()
+        return method(**self.params)
 
 
-class ReadStep(Step):
+class ReadStep(FunctionStep):
     def __init__(self, data):
         super().__init__(data)
-        self.read = data['read']
         self.print = data.get('print')
         self.save_to_file = data.get('save_to_file')
+        self.buffer_size = self.params.get('buffer_size')
 
     @staticmethod
     def from_dict(data: dict) -> 'ReadStep':
         return ReadStep(data)
     
     def execute(self, instrument: Instrument):
-        response = instrument.read_response(self.read)
+        response = super().execute(instrument)
         if self.print:
             print(response)
         if self.save_to_file:
@@ -107,17 +109,14 @@ class ReadStep(Step):
 class MeasureVoltage(FunctionStep):
     def __init__(self, data):
         super().__init__(data)
-        self.reading_times = data.get('reading_times')
-        self.interval = data.get('interval')
+        self.reading_times = self.params.get('reading_times')
+        self.interval = self.params.get('interval')
 
     def validate(self):
         super().validate()
-        if self.reading_times < 1:
+        if self.reading_times and self.reading_times < 1:
             raise ValueError('reading_times must be >= 1')
         
     @staticmethod
     def from_dict(data: dict) -> 'MeasureVoltage':
         return MeasureVoltage(data)
-    
-    def execute(self, instrument: Instrument):
-        instrument.measure_voltage(self.reading_times, self.interval)
