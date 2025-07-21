1c4d65edfc9d4ac62571295dfa26e1fca979ac75
Moein Arabi <moein.mo81@gmail.com>
2025-07-21 09:52:15 +0330

    Refactor code
    - Add set reading burst function
    - Extract a common configuration function

--- devices/hp3458a.py
+++ devices/hp3458a.py
@@ -15,19 +15,6 @@ class HP3458A(Instrument):
     def beep(self):
         self.send_command('BEEP')
         
-    # TODO: Remove this function after testing new ones.
-    def measure_voltage(self, reading_times: int = 1, interval: float = 1.0):
-        """
-        Args:
-            reading_times (int): Number of readings to take.
-            interval (float): Time interval between readings in seconds.
-        """
-        self.send_command('FUNC DCV')
-        self.send_command('MEM FIFO') # Enable memory. remove all existing data in memory
-        self.send_command(f'TIMER {interval}')
-        self.send_command(f'NRDGS {reading_times},TIMER')
-        self.send_command('TRIG SGL')
-        
     def external_buffer(self, enabled: bool):
         command = 'TBUFF ON' if enabled else 'TBUFF OFF'
         self.send_command(command)
@@ -59,8 +46,9 @@ class HP3458A(Instrument):
 
     def get_reading(self):
         """Triggers a single reading and returns the value."""
-        self.send_command('TRIG SGL') # Trigger reading
-        return float(self.read_response())
+        self.send_command('TRIG SGL')
+        response = list(map(float, self.read_response().strip().split()))
+        return response if len(response) > 1 else response[0]
     
     # --- Helper & Configuration Functions ---
     # TODO: Implement interrupt before reading.
@@ -69,7 +57,6 @@ class HP3458A(Instrument):
         """Enables/disables the low-pass filter with the -3dB point at 75kHz."""
         self.send_command('LFILTER ON' if enable else 'LFILTER OFF')
 
-    # TODO: test this function. Is SGL also hold?
     def set_triggering(self, source='SGL', arm_source='AUTO'):
         """
         Sets the trigger and trigger arming source.
@@ -86,19 +73,19 @@ class HP3458A(Instrument):
         self.send_command(f'TARM {arm_source}')
         self.send_command(f'TRIG {source}')
 
-    # TODO: test this. I think it needs MEM FIFO enabled.
-    def set_reading_burst(self, count: int, interval: float = None):
+    def __set_reading_burst(self, count: int, interval: float | None):
         """
         Configures the instrument to take a burst of readings.
 
         Args:
             count (int): The number of readings to take per trigger.
-            interval (float, optional): The time interval between readings in seconds.
+            interval (float | None): The time interval between readings in seconds.
                                         If provided, the sample event is set to TIMER.
                                         Defaults to None, which uses AUTO event.
         
         SCPI Commands: NRDGS, TIMER
         """
+        self.send_command('MEM FIFO')
         if interval:
             self.send_command(f'TIMER {interval}')
             self.send_command(f'NRDGS {count},TIMER')
@@ -112,77 +99,70 @@ class HP3458A(Instrument):
             self.send_command(f'RANGE {mrange:0.6f}')
         self.send_command(f'NPLC {nplc:0.3f}')
 
-    def __autoZero(self, enabled=True):
+    def __autoZero(self, enabled):
+        """The auto zero function applies only to DC voltage, DC current, and resistance measurements."""
         self.send_command('AZERO ON' if enabled else 'AZERO OFF')
 
-    def __hiZ(self, enabled=True):
+    def __hiZ(self, enabled):
+        """the fixed input resistance function for DC voltage measurements"""
         self.send_command('FIXEDZ OFF' if enabled else 'FIXEDZ ON')
 
-    def __ocomp(self, enabled=True):
+    def __ocomp(self, enabled):
         self.send_command('OCOMP ON' if enabled else 'OCOMP OFF')
 
     # --- Measurement Configuration Functions ---
+    
+    def _common_configure(self, 
+                          mrange, 
+                          nplc,
+                          AutoZero=True, 
+                          HiZ=False, 
+                          OffsetCompensation=False, 
+                          count=1,
+                          interval=None):
+        self.send_command('NDIG 6')
+        self.__set_range(mrange, nplc)
+        self.__autoZero(AutoZero)
+        self.__hiZ(HiZ)
+        self.__ocomp(OffsetCompensation)
+        self.__set_reading_burst(count, interval)
 
-    def conf_function_DCV(self, mrange=None, nplc=1, AutoZero=True, HiZ=True):
+    def conf_function_DCV(self, mrange=None, nplc=1, AutoZero=True, HiZ=False, count=1, interval=None):
         """Configures the meter to measure DCV. If range=None the meter is set to Autorange."""
         self.send_command('PRESET NORM')
         self.send_command('DCV')
-        self.send_command('NDIG 6')
-        self.send_command('TRIG SGL')
-        self.__set_range(mrange, nplc)
-        self.__autoZero(AutoZero)
-        self.__hiZ(HiZ)
+        self._common_configure(mrange, nplc, AutoZero, HiZ, count, interval)
 
-    def conf_function_DCI(self, mrange=None, nplc=1, AutoZero=True, HiZ=True):
+    def conf_function_DCI(self, mrange=None, nplc=1, AutoZero=True, HiZ=False, count=1, interval=None):
         """Configures the meter to measure DCI. If range=None the meter is set to Autorange."""
         self.send_command('PRESET NORM')
         self.send_command('DCI')
-        self.send_command('NDIG 6')
-        self.send_command('TRIG SGL')
-        self.__set_range(mrange, nplc)
-        self.__autoZero(AutoZero)
-        self.__hiZ(HiZ)
+        self._common_configure(mrange, nplc, AutoZero, HiZ, count, interval)
 
-    def conf_function_ACV(self, mrange=None, nplc=1, AutoZero=True, HiZ=True):
+    def conf_function_ACV(self, mrange=None, nplc=1, count=1, interval=None):
         """Configures the meter to measure ACV (True RMS). If range=None the meter is set to Autorange."""
         self.send_command('PRESET NORM')
         self.send_command('ACV')
         self.send_command('SETACV SYNC')
-        self.send_command('NDIG 6')
-        self.send_command('TRIG SGL')
-        self.__set_range(mrange, nplc)
-        self.__autoZero(AutoZero)
-        self.__hiZ(HiZ)
+        self._common_configure(mrange, nplc, count=count, interval=interval)
 
-    def conf_function_ACI(self, mrange=None, nplc=1, AutoZero=True, HiZ=True):
+    def conf_function_ACI(self, mrange=None, nplc=1, count=1, interval=None):
         """Configures the meter to measure ACI. If range=None the meter is set to Autorange."""
         self.send_command('PRESET NORM')
         self.send_command('ACI')
-        self.send_command('NDIG 6')
-        self.send_command('TRIG SGL')
-        self.__set_range(mrange, nplc)
-        self.__autoZero(AutoZero)
-        self.__hiZ(HiZ)
+        self._common_configure(mrange, nplc, count=count, interval=interval)
 
-    def conf_function_OHM2W(self, mrange=None, nplc=1, AutoZero=True, OffsetCompensation=True):
+    def conf_function_OHM2W(self, mrange=None, nplc=1, AutoZero=True, OffsetCompensation=False, count=1, interval=None):
         """Configures the meter to measure OHM2W. If range=None the meter is set to Autorange."""
         self.send_command('PRESET NORM')
         self.send_command('OHM')
-        self.send_command('NDIG 6')
-        self.send_command('TRIG SGL')
-        self.__set_range(mrange, nplc)
-        self.__autoZero(AutoZero)
-        self.__ocomp(OffsetCompensation)
+        self._common_configure(mrange, nplc, AutoZero, OffsetCompensation, count=count, interval=interval)
 
-    def conf_function_OHM4W(self, mrange=None, nplc=1, AutoZero=True, OffsetCompensation=True):
+    def conf_function_OHM4W(self, mrange=None, nplc=1, AutoZero=True, OffsetCompensation=False, count=1, interval=None):
         """Configures the meter to measure OHM4W. If range=None the meter is set to Autorange."""
         self.send_command('PRESET NORM')
         self.send_command('OHMF')
-        self.send_command('NDIG 6')
-        self.send_command('TRIG SGL')
-        self.__set_range(mrange, nplc)
-        self.__autoZero(AutoZero)
-        self.__ocomp(OffsetCompensation)
+        self._common_configure(mrange, nplc, AutoZero, OffsetCompensation, count=count, interval=interval)
         
     def conf_function_FREQ(self, mrange='AUTO', gate_time=1.0):
         """
@@ -214,9 +194,8 @@ class HP3458A(Instrument):
         self.send_command('FSOURCE ACV')
         # Use the FUNC command for a concise setup
         self.send_command(f'FUNC FREQ, {range_param}, {resolution_param}')
-        self.send_command('TRIG SGL')
 
-    def conf_function_ACDCV(self, mrange=None, nplc=1, ac_bandwidth_low=20, HiZ=True):
+    def conf_function_ACDCV(self, mrange=None, nplc=1, ac_bandwidth_low=20, HiZ=False):
         """
         Configures the meter to measure combined AC+DC Voltage (True RMS).
 
@@ -232,7 +211,6 @@ class HP3458A(Instrument):
         self.send_command('ACDCV')
         self.send_command(f'ACBAND {ac_bandwidth_low}')
         self.send_command('NDIG 6')
-        self.send_command('TRIG SGL')
         self.__set_range(mrange, nplc)
         self.__autoZero(True)
         self.__hiZ(HiZ)
