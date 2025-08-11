66d34933a58d63711d05a0d1ade5b45c3030d584
Moein Arabi <moein.mo81@gmail.com>
2025-08-11 16:11:12 +0330

    Add pyvisa proxy test code

---
+++ test_pyvisa_proxy.py
@@ -0,0 +1,9 @@
+import pyvisa
+
+
+rm = pyvisa.ResourceManager("192.168.1.102:5000@proxy")
+inst = rm.open_resource("GPIB0::2::INSTR")
+# instr.write('END ON')
+# instr.write('TRIG HOLD')
+inst.write('END ALWAYS')
+print(inst.query('ID?'))
