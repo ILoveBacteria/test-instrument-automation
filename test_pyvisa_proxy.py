93797db68a442b3e0e12c275e0795221b5fbddc6
Moein Arabi <moein.mo81@gmail.com>
2025-08-11 22:04:59 +0330

    Add a TODO to test_pyvisa_proxy file

--- test_pyvisa_proxy.py
+++ test_pyvisa_proxy.py
@@ -2,6 +2,7 @@ import pyvisa
 
 
 rm = pyvisa.ResourceManager("192.168.1.102:5000@proxy")
+# TODO: termination in constructor
 inst = rm.open_resource("GPIB0::2::INSTR")
 # instr.write('END ON')
 # instr.write('TRIG HOLD')
