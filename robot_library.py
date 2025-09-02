d4b40257766c19bc5c04caffb393f9cf1130db71
Moein Arabi <moein.mo81@gmail.com>
2025-09-02 09:46:03 +0330

    Add initialize data publishing

--- robot_library.py
+++ robot_library.py
@@ -10,19 +10,23 @@ class BaseLibrary:
         self._r = None
         try:
             self._r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+            self.publish(self.publish_format(0.0))
         except Exception as e:
             print(e)
 
     def publish(self, event: dict):
         self._r.publish('robot_events', json.dumps(event))
+        
+    def publish_format(self, result: float) -> dict:
+        return {'type': 'data', 'owner': self.NAME, 'data': result}
     
     
 def publish_result(func):
     @functools.wraps(func)
-    def wrapper(self, *args, **kwargs):
+    def wrapper(self: BaseLibrary, *args, **kwargs):
         result = func(self, *args, **kwargs)
         # publish to Redis
         if self._r:
-            self.publish({'type': 'data', 'owner': self.NAME, 'data': result})
+            self.publish(self.publish_format(result))
         return result
     return wrapper
